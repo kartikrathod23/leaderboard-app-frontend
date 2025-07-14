b5bd3ddc3a7f6978e95424f1f17276432859d859
kartik rathod <rathodkartik293@gmail.com>
2025-07-14 16:27:18 +0530

    backend url changed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js - Central place to manage API URL for frontend
 
-const API_URL = "http://localhost:5000/api";
-// const API_URL = "https://leaderboard-app-backend-p30q.onrender.com/api";
+// const API_URL = "http://localhost:5000/api";
+const API_URL = "https://leaderboard-app-backend-p30q.onrender.com/api";
 
 export default API_URL; 
