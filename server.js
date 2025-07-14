39ed55efa56474b91996352bf9c0a5cde969050f
kartik rathod <rathodkartik293@gmail.com>
2025-07-14 15:45:26 +0530

    updated backend url

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // server.js - Central place to manage API URL for frontend
 
-const API_URL = "http://localhost:5000/api";
+// const API_URL = "http://localhost:5000/api";
+const API_URL = "https://leaderboard-app-backend.vercel.app/api";
 
 export default API_URL; 
